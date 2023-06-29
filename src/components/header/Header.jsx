9c5a1bfea84b6763598d01f725fc61b0d3133816
Visinho <Elvisvisinho@rocketmail.com>
2023-06-29 21:17:53 +0100

    Designing the header component

--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -1,11 +1,18 @@
 import React from 'react';
 import "./header.css";
+import CTA from './CTA';
 
 const Header = () => {
   return (
-    <div>
-      Header
-    </div>
+    <header>
+      <div className="container header__container">
+        <h5>Hello, My name is</h5>
+        <h1>Elvis Njoku</h1>
+        <h5 className="text-light">Fullstact Developer</h5>
+        <CTA />
+      </div>
+    </header>
+
   )
 }
 
