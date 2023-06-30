2051967f1e72d620dc2922bf6ffb91aef6c44562
Visinho <Elvisvisinho@rocketmail.com>
2023-06-30 09:40:58 +0100

    Worked on the floating nav compnent

--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -15,7 +15,7 @@ const Header = () => {
         <HeaderSocials />
 
         <div className='me'>
-          <img src={ImageVisinho} alt='me'/>
+          <img src={ImageVisinho} alt='me' className='styling'/>
         </div>
 
         <a href='#contact' className='scroll__down'>Scroll Down</a>
